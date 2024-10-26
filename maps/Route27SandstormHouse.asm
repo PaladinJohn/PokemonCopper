dbfc530f61d68405be49e77e4671aa5ece644783
PaladinJohn <Tigerquan@aol.com>
2024-10-26 03:58:30 -0400

    Added Magnitude TM
    Replaced the Mud Slap TM with Magnitude.

--- maps/Route27SandstormHouse.asm
+++ maps/Route27SandstormHouse.asm
@@ -20,7 +20,7 @@ SandstormHouseWoman:
 .Loyal:
 	writetext SandstormHouseWomanLoyalText
 	promptbutton
-	verbosegiveitem TM_SANDSTORM
+	verbosegiveitem TM_ATTRACT
 	iffalse .Done
 	setevent EVENT_GOT_TM37_SANDSTORM
 .AlreadyGotItem:
