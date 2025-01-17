017d49082b3adce3c0e6afb1602093a9c3a8bb15
PaladinJohn <Tigerquan@aol.com>
2025-01-17 02:06:39 -0500

    Updated Pokemon Sprites 4
    Finished Implementing Shinx.
    
    Implemented Rival battle sprite.

--- gfx/pokemon/shinx/anim.asm
+++ gfx/pokemon/shinx/anim.asm
@@ -1,5 +1,10 @@
+	frame 0, 09
+	frame 1, 08
+	setrepeat 5
+	frame 2, 02
+	frame 3, 02
+	frame 2, 02
+	frame 1, 02
+	dorepeat 3
 	frame 0, 06
-	frame 1, 22
-	frame 2, 12
-	frame 4, 08
 	endanim
--- gfx/pokemon/shinx/anim_idle.asm
+++ gfx/pokemon/shinx/anim_idle.asm
@@ -1,5 +1,6 @@
+	frame 4, 10
 	setrepeat 2
-	frame 0, 04
-	frame 3, 06
-	dorepeat 1
+	frame 5, 06
+	frame 6, 06
+	dorepeat 2
 	endanim
