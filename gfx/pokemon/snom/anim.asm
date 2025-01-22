6a660d53dcedef6e6247c2a31948abd43e4d8fb1
PaladinJohn <Tigerquan@aol.com>
2025-01-22 01:35:07 -0500

    Updated Pokemon Sprites 7
    Implemented Snom sprites.

--- gfx/pokemon/snom/anim.asm
+++ gfx/pokemon/snom/anim.asm
@@ -1,15 +1,7 @@
-	frame 2, 10
-	frame 0, 10
-	frame 2, 10
-	frame 0, 06
-	frame 1, 06
-	frame 0, 06
 	setrepeat 3
-	frame 1, 04
-	frame 0, 04
-	dorepeat 7
-	setrepeat 2
-	frame 1, 02
-	frame 0, 02
-	dorepeat 11
+	frame 0, 09
+	frame 1, 07
+	frame 0, 06
+	dorepeat 1
+	frame 2, 07
 	endanim
--- gfx/pokemon/snom/anim_idle.asm
+++ gfx/pokemon/snom/anim_idle.asm
@@ -1,5 +1,8 @@
 	setrepeat 2
-	frame 0, 12
-	frame 2, 16
+	frame 0, 07
+	frame 1, 07
+	frame 3, 04
+	frame 1, 07
 	dorepeat 1
+	frame 2, 07
 	endanim
