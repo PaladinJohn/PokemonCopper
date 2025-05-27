5cda95c14e598847c568c67ea5149872adfa790d
PaladinJohn <Tigerquan@aol.com>
2025-05-27 03:35:41 -0400

    Updated Mom Decorations
    Reduced the total price of decorations from mom. More work still needs to be done to make this viable.

--- data/items/mom_phone.asm
+++ data/items/mom_phone.asm
@@ -15,52 +15,52 @@ MomItems_1:
 
 MomItems_2:
 	momitem    900,   600, MOM_ITEM, SUPER_POTION
-	momitem   2000,  1000, MOM_DOLL, DECO_JIGGLYPUFF_DOLL ; Mystery Gift
+	;momitem   2000,  1000, MOM_DOLL, DECO_JIGGLYPUFF_DOLL ; Mystery Gift/Need to put elsewhere.
 	momitem   3000,  1000, MOM_DOLL, DECO_EEVEE_DOLL ; Mystery Gift
 	momitem   4000,   270, MOM_ITEM, REPEL
 	momitem   5000,  1000, MOM_DOLL, DECO_SPRIGATITO_DOLL ; Mystery Gift
 	momitem   6000,  1000, MOM_DOLL, DECO_STARYU_DOLL ; Mystery Gift
 	momitem   7000,   600, MOM_ITEM, SUPER_POTION
+	momitem   8000,  1000, MOM_DOLL, DECO_MAGIKARP_DOLL ; Mystery Gift
+	momitem   9000,  1800, MOM_DOLL, DECO_ODDISH_DOLL ; Mystery Gift
 	momitem  10000,  1800, MOM_DOLL, DECO_CHARMANDER_DOLL
-	momitem  12000,  1800, MOM_DOLL, DECO_MAGIKARP_DOLL ; Mystery Gift
+	momitem  12000,  2000, MOM_DOLL, DECO_GENGAR_DOLL ; Mystery Gift
+	momitem  14000,  2000, MOM_DOLL, DECO_SHELLDER_DOLL ; Mystery Gift
 	momitem  15000,  3000, MOM_ITEM, MOON_STONE
-	momitem  17000,  1800, MOM_DOLL, DECO_ODDISH_DOLL ; Mystery Gift
+	momitem  18000,  3000, MOM_DOLL, DECO_GRIMER_DOLL ; Mystery Gift
 	momitem  19000,   600, MOM_ITEM, SUPER_POTION
-	momitem  21000,  1800, MOM_DOLL, DECO_GENGAR_DOLL ; Mystery Gift
-	momitem  23000,  1800, MOM_DOLL, DECO_SHELLDER_DOLL ; Mystery Gift
-	momitem  26000,  1800, MOM_DOLL, DECO_GRIMER_DOLL ; Mystery Gift
+	momitem  21000,  3000, MOM_DOLL, DECO_VOLTORB_DOLL ; Mystery Gift
+	momitem  24000,  3000, MOM_DOLL, DECO_CLEFAIRY_POSTER ; Mystery Gift
+	momitem  27000,  3000, MOM_DOLL, DECO_JIGGLYPUFF_POSTER ; Mystery Gift
 	momitem  30000,  4800, MOM_DOLL, DECO_CLEFAIRY_DOLL
-	momitem  33000,  4800, MOM_DOLL, DECO_VOLTORB_DOLL ; Mystery Gift
-	momitem  36000,  4800, MOM_DOLL, DECO_CLEFAIRY_POSTER ; Mystery Gift
+	momitem  34000,  4800, MOM_DOLL, DECO_SNES ; Mystery Gift
+	momitem  38000,  4800, MOM_DOLL, DECO_CYNDAQUIL_DOLL ; Mystery Gift
 	momitem  40000,   900, MOM_ITEM, HYPER_POTION
-	momitem  43000,  4800, MOM_DOLL, DECO_JIGGLYPUFF_POSTER ; Mystery Gift
+	momitem  48000,  4800, MOM_DOLL, DECO_GEODUDE_DOLL ; Mystery Gift
 	momitem  50000,  8000, MOM_DOLL, DECO_TOTODILE_DOLL
-	momitem  54000,  8000, MOM_DOLL, DECO_SNES ; Mystery Gift
-	momitem  58000,  8000, MOM_DOLL, DECO_CYNDAQUIL_DOLL ; Mystery Gift
-	momitem  62000,  8000, MOM_DOLL, DECO_GEODUDE_DOLL ; Mystery Gift
-	momitem  66000,  8000, MOM_DOLL, DECO_MACHOP_DOLL ; Mystery Gift
-	momitem  71000, 10000, MOM_DOLL, DECO_MAGNAPLANT ; Mystery Gift
-	momitem  76000, 10000, MOM_DOLL, DECO_TROPICPLANT ; Mystery Gift
-	momitem  81000, 10000, MOM_DOLL, DECO_FAMICOM ; Mystery Gift
-	momitem  86000, 10000, MOM_DOLL, DECO_N64 ; Mystery Gift
-	momitem  92000, 12000, MOM_DOLL, DECO_BULBASAUR_DOLL ; Mystery Gift
+	momitem  60000,  8000, MOM_DOLL, DECO_MACHOP_DOLL ; Mystery Gift
+	momitem  67000,  8000, MOM_DOLL, DECO_MAGNAPLANT ; Mystery Gift
+	momitem  75000,  8000, MOM_DOLL, DECO_TROPICPLANT ; Mystery Gift
+	momitem  83000,  8000, MOM_DOLL, DECO_FAMICOM ; Mystery Gift
+	momitem  92000,  9000, MOM_DOLL, DECO_N64 ; Mystery Gift
 	momitem 100000, 22800, MOM_DOLL, DECO_BIG_SNORLAX_DOLL
-	momitem 106000, 12000, MOM_DOLL, DECO_SQUIRTLE_DOLL ; Mystery Gift
-	momitem 112000, 12000, MOM_DOLL, DECO_PINK_BED ; Mystery Gift
-	momitem 118000, 12000, MOM_DOLL, DECO_POLKADOT_BED ; Mystery Gift
-	momitem 125000, 14000, MOM_DOLL, DECO_RED_CARPET ; Mystery Gift
-	momitem 132000, 14000, MOM_DOLL, DECO_BLUE_CARPET ; Mystery Gift
-	momitem 139000, 14000, MOM_DOLL, DECO_YELLOW_CARPET ; Mystery Gift
-	momitem 146000, 14000, MOM_DOLL, DECO_GREEN_CARPET ; Mystery Gift
-	momitem 154000, 16000, MOM_DOLL, DECO_JUMBOPLANT ; Mystery Gift
-	momitem 162000, 16000, MOM_DOLL, DECO_VIRTUAL_BOY ; Mystery Gift
+	momitem 105000,  9000, MOM_DOLL, DECO_BULBASAUR_DOLL ; Mystery Gift
+	momitem 106000,  9000, MOM_DOLL, DECO_SQUIRTLE_DOLL ; Mystery Gift
+	momitem 112000,  9000, MOM_DOLL, DECO_PINK_BED ; Mystery Gift
+	momitem 118000, 10000, MOM_DOLL, DECO_POLKADOT_BED ; Mystery Gift
+	momitem 125000, 10000, MOM_DOLL, DECO_RED_CARPET ; Mystery Gift
+	momitem 132000, 10000, MOM_DOLL, DECO_BLUE_CARPET ; Mystery Gift
+	momitem 139000, 10000, MOM_DOLL, DECO_YELLOW_CARPET ; Mystery Gift
+	momitem 146000, 12000, MOM_DOLL, DECO_GREEN_CARPET ; Mystery Gift
+	momitem 154000, 12000, MOM_DOLL, DECO_JUMBOPLANT ; Mystery Gift
+	momitem 162000, 12000, MOM_DOLL, DECO_VIRTUAL_BOY ; Mystery Gift
 	momitem 170000, 22800, MOM_DOLL, DECO_BIG_ONIX_DOLL ; Mystery Gift
-	momitem 178000, 16000, MOM_DOLL, DECO_BULBASAUR_POSTER ; Mystery Gift
+	momitem 178000, 12000, MOM_DOLL, DECO_BULBASAUR_POSTER ; Mystery Gift
 	momitem 187000, 22800, MOM_DOLL, DECO_BIG_LAPRAS_DOLL ; Mystery Gift
-	momitem 196000, 18000, MOM_DOLL, DECO_SURF_PIKACHU_DOLL ; Mystery Gift
-	momitem 205000, 18000, MOM_DOLL, DECO_FLAREON_BED ; Mystery Gift
-	momitem 214000, 18000, MOM_DOLL, DECO_UNOWN_DOLL ; Mystery Gift
-	momitem 224000, 20000, MOM_DOLL, DECO_TENTACOOL_DOLL ; Mystery Gift
+	momitem 196000, 14000, MOM_DOLL, DECO_SURF_PIKACHU_DOLL ; Mystery Gift
+	momitem 205000, 14000, MOM_DOLL, DECO_FLAREON_BED ; Mystery Gift
+	momitem 214000, 14000, MOM_DOLL, DECO_UNOWN_DOLL ; Mystery Gift
+	momitem 224000, 14000, MOM_DOLL, DECO_TENTACOOL_DOLL ; Mystery Gift
 .End
 
 	dt 0 ; unused
