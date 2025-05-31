33e540e71881722d0860f82ea835d41ca8ef23be
PaladinJohn <Tigerquan@aol.com>
2025-05-30 22:23:42 -0400

    Updated Spiritomb Sprites
    Spiritomb sprites updated.
    
    Credit: Nuuk

--- gfx/pokemon/spiritomb/anim.asm
+++ gfx/pokemon/spiritomb/anim.asm
@@ -1,8 +1,12 @@
+	frame 0, 04
+	setrepeat 2
 	frame 1, 12
-	frame 4, 05
-	setrepeat 4
+	frame 2, 12
+	frame 3, 12
+	dorepeat 2
+	setrepeat 2
+	frame 1, 08
 	frame 2, 08
 	frame 3, 08
-	dorepeat 3
-	frame 4, 25
+	dorepeat 7
 	endanim
--- gfx/pokemon/spiritomb/anim_idle.asm
+++ gfx/pokemon/spiritomb/anim_idle.asm
@@ -1,6 +1,6 @@
-	setrepeat 2
-	frame 4, 08
+	frame 4, 12
+	frame 5, 12
+	frame 4, 12
 	frame 0, 08
-	dorepeat 1
-	frame 4, 30
+	frame 5, 08
 	endanim
