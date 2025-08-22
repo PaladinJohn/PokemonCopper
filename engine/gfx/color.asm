177b8b716954ce40a223e1061234bad16841c4af
PaladinJohn <Tigerquan@aol.com>
2025-08-22 04:11:10 -0400

    Increased Shiny Rate
    Shiny rate is now roughly ~1:2700, or 3:8192.
    
    3/4 of all Attack DVs and 1/2 of all Defense DVs result in a shiny.

--- engine/gfx/color.asm
+++ engine/gfx/color.asm
@@ -1,7 +1,7 @@
 INCLUDE "engine/gfx/sgb_layouts.asm"
 
-;DEF SHINY_ATK_MASK EQU %0010
-DEF SHINY_DEF_DV EQU 10
+DEF SHINY_ATK_MASK EQU %0011
+DEF SHINY_DEF_MASK EQU %0010
 DEF SHINY_SPD_DV EQU 10
 DEF SHINY_SPC_DV EQU 10
 
@@ -12,16 +12,15 @@ CheckShininess:
 	ld l, c
 	ld h, b
 
-; Attack; Attack is no longer checked for Shiny.
-	;ld a, [hl]
-	;and SHINY_ATK_MASK << 4
-	;jr z, .not_shiny
+; Attack
+	ld a, [hl]
+	and SHINY_ATK_MASK << 4
+	jr z, .not_shiny
 
 ; Defense
 	ld a, [hli]
-	and %1111
-	cp SHINY_DEF_DV
-	jr nz, .not_shiny
+	and SHINY_DEF_MASK
+	jr z, .not_shiny
 
 ; Speed
 	ld a, [hl]
